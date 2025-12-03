108e7aeafa2f16634e3c3a2cacad135671570b59
Ardra1872 <ardrasnaircpdy@gmail.com>
2025-12-03 20:18:22 +0530

    Home page created

---
+++ public/assets/script.js
@@ -0,0 +1,11 @@
+// // After animation, show landing and change bg
+// setTimeout(function() {
+//     // Add class to change background color
+//     document.body.classList.add('splash-done');
+
+//     // Hide splash content
+//     document.getElementById('splash').style.display = 'none';
+
+//     // Show landing buttons
+//     document.getElementById('landing').classList.remove('hidden');
+// }, 1500);
